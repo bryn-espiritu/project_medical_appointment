abe8b960411fefa314c65b515229bd40a632e5e3
brynex20 <brynespiritu@gmail.com>
2022-10-28 15:09:25 +0800

    remove comment

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   resources :appointments do
   resources :notes, except: :show
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
    root "appointments#index"
 
 end
