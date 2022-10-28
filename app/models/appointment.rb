9b866798f929add70d78356dfb4fb8015f6502c1
brynex20 <brynespiritu@gmail.com>
2022-10-28 12:17:19 +0800

    appointment: added relationship (has_many) to notes

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,10 @@
 class Appointment < ApplicationRecord
+  # default_scope { where(delete_at: nil) }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
 
   belongs_to :user
+  has_many :notes
 end
