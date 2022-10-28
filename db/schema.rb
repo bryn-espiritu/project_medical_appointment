d57ec753c388f1e6106cde0c123e1221b03b346b
brynex20 <brynespiritu@gmail.com>
2022-10-28 12:15:11 +0800

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_27_063702) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_27_091731) do
   create_table "appointments", charset: "utf8mb4", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -20,6 +20,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_063702) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_appointments_on_user_id"
+  end
+
+  create_table "notes", charset: "utf8mb4", force: :cascade do |t|
+    t.string "subject"
+    t.string "content"
+    t.bigint "appointment_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["appointment_id"], name: "index_notes_on_appointment_id"
+    t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
